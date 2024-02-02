ba02510b52f77f8fb0270c45b710305c90e8fa46
Hoang Nghia Khue <62629086+robin5hnk@users.noreply.github.com>
2024-02-02 22:04:46 +0700

    Update main.cpp

--- LeetCode 75/Array - String/1431. Kids With the Greatest Number of Candies/main.cpp
+++ LeetCode 75/Array - String/1431. Kids With the Greatest Number of Candies/main.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> result;
+        int max = candies[0];
 
+        for (int i = 1; i < candies.size(); i++) {
+            if (candies[i] > max) {
+                max = candies[i];
+            }
+        }
+
+        for (int i = 0; i < candies.size(); i++) {
+            if (candies[i] + extraCandies >= max) {
+                result.push_back(true);
+            } else {
+                result.push_back(false);
+            }
+        }
+
+        return result;
+    }
+};

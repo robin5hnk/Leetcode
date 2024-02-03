8aac79c9fee738495c66f4436dd48fe949b1f2ea
Hoang Nghia Khue <62629086+robin5hnk@users.noreply.github.com>
2024-02-03 18:13:30 +0700

    Update main.cpp

--- LeetCode 75/Array - String/605. Can Place Flowers/main.cpp
+++ LeetCode 75/Array - String/605. Can Place Flowers/main.cpp
@@ -1,1 +1,41 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        bool result;
 
+        if (flowerbed.size() == 1) {
+            if (flowerbed[0] == 0 && n <= 1) {
+                return true;
+            } 
+            else if (flowerbed[0] == 1 && n <= 0) {
+                return true;
+            } 
+            else {
+                return false;
+            }
+        }
+
+        if (flowerbed[0] == 0 && flowerbed[1] == 0) {
+            n--;
+            flowerbed[0] = 1;
+        }
+
+        for (int i = 2; i < flowerbed.size() - 1; i++) {
+            if (flowerbed[i - 1] == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
+                n--;
+                flowerbed[i] = 1;
+            }
+        }
+
+        if (flowerbed[flowerbed.size() - 1] == 0 && flowerbed[flowerbed.size() - 2] == 0) {
+            n--;
+            flowerbed[flowerbed.size() - 1] = 1;
+        }
+
+        if (n <= 0) {
+            return true;
+        }
+
+        return false;
+    }
+};

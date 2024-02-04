f7960772afdbd14c5897e0cec930fa7be6029bc1
Hoang Nghia Khue <62629086+robin5hnk@users.noreply.github.com>
2024-02-04 13:31:23 +0700

    Update main.cpp

--- LeetCode 75/Array - String/238. Product of Array Except Self/main.cpp
+++ LeetCode 75/Array - String/238. Product of Array Except Self/main.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n, 1);
+        vector<int> leftProduct(n, 1);
+        vector<int> rightProduct(n, 1);
 
+        for (int i = 1; i < nums.size(); i++) {
+            leftProduct[i] = leftProduct[i - 1] * nums[i - 1];
+        }
+
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            rightProduct[i] = rightProduct[i + 1] * nums[i + 1];
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            result[i] = leftProduct[i] * rightProduct[i];
+        }
+
+        return result;
+    }
+};

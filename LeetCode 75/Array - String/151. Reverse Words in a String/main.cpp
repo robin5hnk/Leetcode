2c4ece9787685cb72f1bac74f9e8cb93d56cb9fe
Hoang Nghia Khue <62629086+robin5hnk@users.noreply.github.com>
2024-02-04 11:36:36 +0700

    Update main.cpp

--- LeetCode 75/Array - String/151. Reverse Words in a String/main.cpp
+++ LeetCode 75/Array - String/151. Reverse Words in a String/main.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string temp = "";
+        vector<string> v;
 
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] != ' ') {
+                temp += s[i];
+            }
+            if ((s[i] == ' ' && temp != "" ) || (i == s.size() - 1 && s[i] != ' ')) {
+                v.push_back(temp);
+                temp = "";
+            }
+        }
+
+        temp = "";
+        for (int i = v.size() - 1; i >= 0; i--) {
+            if (i != 0) {
+                temp += v[i] + " ";
+
+            } else {
+                temp += v[i];
+            }
+        }
+
+        return temp;
+    }
+};

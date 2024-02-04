f29e08a6438a4d54f2d99d04769c0d1dc4c38c39
Hoang Nghia Khue <62629086+robin5hnk@users.noreply.github.com>
2024-02-04 10:11:38 +0700

    Update main.cpp

--- LeetCode 75/Array - String/345. Reverse Vowels of a String/main.cpp
+++ LeetCode 75/Array - String/345. Reverse Vowels of a String/main.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        stack<char> vowels;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'u' || s[i] == 'i' || s[i] == 'o' ||
+            s[i] == 'A' || s[i] == 'E' || s[i] == 'U' || s[i] == 'I' || s[i] == 'O') {
+                vowels.push(s[i]);
+            }
+        }
 
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'u' || s[i] == 'i' || s[i] == 'o' ||
+            s[i] == 'A' || s[i] == 'E' || s[i] == 'U' || s[i] == 'I' || s[i] == 'O') {
+                s[i] = vowels.top();
+                vowels.pop();
+            }
+        }
+
+        return s;
+    }
+};

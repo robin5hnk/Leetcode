84a85d4d6d151cf6b956ca904e42e96677c991f8
Hoang Nghia Khue <62629086+robin5hnk@users.noreply.github.com>
2024-02-05 22:34:00 +0700

    Update main.cpp

--- LeetCode 75/Two Pointers/283. Move Zeroes/main.cpp
+++ LeetCode 75/Two Pointers/283. Move Zeroes/main.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int count = 0;
+        for (vector<int>::iterator it = nums.begin(); it != nums.end() ; ) {
+            // Vì sau khi xóa 1 phần tử kích thước vector sẽ thay đổi, nếu phần tử tại it bị xóa thì it sẽ 
+            // trỏ qua phần tử sau mà ko cần it++, vì vậy chỉ cần it++ đối với trường hợp ko bị xóa (!= 0)
+            if (*it == 0) {
+                count++;
+                nums.erase(it);
+            }
+            else {
+                it++;
+            }
+        }
 
+        for (int i = 0; i < count; i++) {
+            nums.push_back(0);
+        }
+    }
+};

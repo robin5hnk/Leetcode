1911690cce888d9e6fe2413ea5372c4bdfb9b021
Hoang Nghia Khue <62629086+robin5hnk@users.noreply.github.com>
2024-02-06 18:56:55 +0900

    Update main.cpp

--- LeetCode 75/Prefix Sum/1732. Find the Highest Altitude/main.cpp
+++ LeetCode 75/Prefix Sum/1732. Find the Highest Altitude/main.cpp
@@ -1,1 +1,16 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        vector<int> altitudes(gain.size() + 1, 0);
+        int max = altitudes[0];
 
+        for (int i = 0; i < gain.size(); i++) {
+            altitudes[i+1] = altitudes[i] + gain[i];
+            if (altitudes[i+1] > max) {
+                max = altitudes[i+1];
+            }
+        }
+
+        return max;
+    }
+};
